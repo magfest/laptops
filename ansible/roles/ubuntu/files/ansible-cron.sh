43e169741e113787e40b28e75d252a2bf76265ac
Aaron Saderholm <2559942+aaronsaderholm@users.noreply.github.com>
2023-01-05 13:49:26 -0500

    Install script, restart network manager

---
+++ ansible/roles/ubuntu/files/ansible-cron.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Hopefully this works.
+#
+if [ "$1" == "--force" ]; then
+    FORCE="-o"
+else
+    FORCE=""
+fi
+
+url='https://github.com/magfest/laptops.git'
+echo "$FILE exists ... comfiguring from laptop repo"
+directory='/opt/laptops'
+requirements=${directory}/repo/ansible/requirements.yaml
+
+
+while ! ping -c1 google.com; do sleep 3; done
+
+ansible-galaxy install -r ${requirements}
+ansible-galaxy collection install -r ${requirements}
+ansible-pull ${FORCE} -C ${checkout} -d ${directory}/repo -i localhost, -U ${url} --tags "laptops" --vault-password-file /opt/ansible/vault-password ansible/playbook.yaml 2>&1 | tee -a ${logfile}
